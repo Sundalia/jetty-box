596af6bd1745f3b83c7dd552ffcc1b3ce76b1a25
Artem <a.v.lyamichev@gmail.com>
2024-03-24 19:52:36 -0300

    fix: slider overflow

--- jetty-box/src/components/Slider.jsx
+++ jetty-box/src/components/Slider.jsx
@@ -4,14 +4,13 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/free-mode";
 
-import { FreeMode, Pagination } from "swiper/modules";
+import { FreeMode } from "swiper/modules";
 
-import { RxArrowTopRight } from "react-icons/rx";
 import { ServiceData } from "./utils/constants.js";
 
 export default function () {
   return (
-    <div className="pt-16 px-4 pb-[89px]">
+    <div className="pt-16 px-4 pb-[89px] overflow-hidden">
       <Swiper
         breakpoints={{
           375: {
@@ -20,12 +19,12 @@ export default function () {
           },
         }}
         freeMode={true}
-        modules={[FreeMode, Pagination]}
-        className="gap-2 [overflow:overlay] "
+        modules={[FreeMode]}
+        className="gap-2 overflow-visible"
       >
         {ServiceData.map((item) => (
           <SwiperSlide key={item.title}>
-            <div className="image-container flex flex-col border border-[#B7B7B7] gap-6 group relative shadow-lg rounded  text-white px-6 py-8 h-[268px] w-[187px] overflow-hidden cursor-pointer">
+          <div className="image-container flex flex-col border border-[#B7B7B7] gap-6 group relative shadow-lg rounded  text-white px-6 py-8 h-[268px] w-[187px] overflow-hidden cursor-pointer">
               <div
                 className="absolute inset-0 bg-cover bg-center"
                 style={{ backgroundImage: `url(${item.backgroundImage})` }}
