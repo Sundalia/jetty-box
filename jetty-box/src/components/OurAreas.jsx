ca98e12e126cedfff1e2e34df6902ee2727ec4a7
Artem <a.v.lyamichev@gmail.com>
2024-03-25 00:48:25 -0300

    feat: OurAreas update

--- jetty-box/src/components/OurAreas.jsx
+++ jetty-box/src/components/OurAreas.jsx
@@ -1,35 +1,48 @@
+import data from "../assets/areas-data.svg";
+import design from "../assets/areas-design.svg";
+import events from "../assets/areas-events.svg";
+import IT from "../assets/areas-IT.svg";
+import logistics from "../assets/areas-logistics.svg";
+import processing from "../assets/areas-processing.svg";
+import repair from "../assets/areas-repair.svg";
+
 function HowWeWork() {
   return (
     <div className="px-4 pb-[56px]">
       <div>
         <h3 className="font-['Plateia'] text-left uppercase text-2xl max-w-[300px] leading-normal">
-          Охватываем широкую нишивую область
+          Охватываем широкую нишевую область
         </h3>
         <div className="flex mt-8">
-          <ul className="flex flex-wrap gap-x-4 gap-y-3">
-            <li className="flex items-center justify-center border text-center border-white rounded rounded-s px-3 py-2">
+          <ul className="grid gap-2 grid-cols-2">
+          <li className="bg-[#1E1E1E] leading-5 w-[168px] h-[172px] gap-4 flex flex-col items-center justify-center border text-center border-[#B7B7B7] border-gradient-to-r from-transparent to-[#545454] rounded rounded-s px-3 py-2">
+              <img src={events} />
               <p>Организация мероприятий</p>
             </li>
-            <li className="flex items-center justify-center border text-center border-white rounded rounded-s px-3 py-2">
-              <p>IT сервисы</p>
-            </li>
-            <li className="flex items-center justify-center border text-center border-white rounded rounded-s px-3 py-2">
+            <li className="bg-[#1E1E1E] leading-5 w-[168px] h-[172px] gap-4 flex flex-col items-center justify-center border text-center border-[#B7B7B7] border-gradient-to-r from-transparent to-[#545454] rounded rounded-s px-3 py-2">
+              <img src={design} />
               <p>Дизайнерские изделия</p>
             </li>
-          </ul>
-        </div>
-        <div className="flex mt-10 ml-5">
-          <ul className="flex flex-col gap-y-5">
-            <li className="text-left">
+
+            <li className="bg-[#1E1E1E] leading-5 w-[168px] h-[172px] gap-4 flex flex-col items-center justify-center border text-center border-[#B7B7B7] border-gradient-to-r from-transparent to-[#545454] rounded rounded-s px-3 py-2">
+              <img src={IT} />
+              <p>IT сервисы</p>
+            </li>
+
+            <li className="bg-[#1E1E1E] leading-5 w-[168px] h-[172px] gap-4 flex flex-col items-center justify-center border text-center border-[#B7B7B7] border-gradient-to-r from-transparent to-[#545454] rounded rounded-s px-3 py-2">
+              <img src={logistics} />
               <p>Логистика</p>
             </li>
-            <li className="text-left">
+            <li className="bg-[#1E1E1E] leading-5 w-[168px] h-[172px] gap-4 flex flex-col items-center justify-center border text-center border-[#B7B7B7] border-gradient-to-r from-transparent to-[#545454] rounded rounded-s px-3 py-2">
+              <img src={data} />
               <p>Анализ данных</p>
             </li>
-            <li className="text-left">
+            <li className="bg-[#1E1E1E] leading-5 w-[168px] h-[172px] gap-4 flex flex-col items-center justify-center border text-center border-[#B7B7B7] border-gradient-to-r from-transparent to-[#545454] rounded rounded-s px-3 py-2">
+              <img src={processing} />
               <p>Переработка вторсырья</p>
             </li>
-            <li className="text-left">
+            <li className="bg-[#1E1E1E] leading-5 w-[168px] h-[172px] gap-4 flex flex-col items-center justify-center border text-center border-[#B7B7B7] border-gradient-to-r from-transparent to-[#545454] rounded rounded-s px-3 py-2">
+              <img src={repair}  />
               <p>Ремонт и производство продукции</p>
             </li>
           </ul>
