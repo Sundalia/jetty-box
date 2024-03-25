4c5293dd773674a6022266236562104c79f3c881
Artem <a.v.lyamichev@gmail.com>
2024-03-25 01:09:03 -0300

    feat: Hero update

--- jetty-box/src/components/Hero.jsx
+++ jetty-box/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import mainbox from "../assets/main-box.svg";
 import mainboxShadow from "../assets/main-box-shadow.svg";
+import magic from "../assets/hero-magic.svg";
 
 function Hero() {
   return (
@@ -12,10 +13,13 @@ function Hero() {
       </h3>
       <img src={mainbox} alt="Описание изображения" className="mt-11 m-auto" />
       <img src={mainboxShadow} alt="Описание изображения" className="m-auto" />
-      <p className="mt-8 font-gilroy text-xs leading-4 text-center">
-        Получите в свой проект системный бокс решений, который увеличит вашу
-        прибыль
-      </p>
+      <div className="mt-8 flex items-center justify-center gap-6">
+        <img src={magic} />
+        <p className="font-gilroy text-xs leading-4 text-left w-[267px]">
+          Получите в свой проект системный бокс решений, который увеличит вашу
+          прибыль
+        </p>
+      </div>
       <div className="p-0.5 button-shadow h-12 rounded-[4px] mt-9 w-full bg-gradient-to-r from-white via-[#7D6137] via-[#7D6137] to-[#7D6137]">
         <div className="back h-full flex bg-[#7D6137] items-center justify-center">
           <span>Получить аудит проекта</span>
