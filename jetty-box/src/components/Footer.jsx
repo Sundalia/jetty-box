d5d783e5859f0d0d2c76872b03843c6152188f74
Artem <a.v.lyamichev@gmail.com>
2024-03-29 16:23:10 -0300

    feat: Footer desktop

--- jetty-box/src/components/Footer.jsx
+++ jetty-box/src/components/Footer.jsx
@@ -6,29 +6,69 @@ import youtube from "../assets/youtube-icon.svg";
 
 function Footer() {
   const { t } = useTranslation();
+  const handleScrollToTop = () => {
+    console.log("handleScrollToTop:");
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
-    <div className="px-4 pb-[127px] bg-[#262525] relative z-10">
-      <div className="h-px w-full bg-gradient-to-r from-transparent via-white to-transparent"></div>
-      <h3 className="mt-4 font-['Plateia'] text-left uppercase text-xl">
-        Logo
-      </h3>
-      <p className="font-['Proxima Nova'] font-light text-base mt-6 text-left">
-        {t("dedugin")}
-      </p>
-      <button className="bg-white mt-4 h-[26px] flex items-center w-[128px] justify-center rounded rounded-s">
-        <img src={telegram} alt="Описание изображения" className="" />
-      </button>
-
-      <div className="flex-row flex mt-[14px] gap-[10px]">
-        <button className="bg-white h-[26px] w-[26px] flex items-center justify-center rounded rounded-s">
-          <img src={instagram} alt="Описание изображения" />
+    <div className="md:inset-x-0 md:absolute w-full px-4 pb-[127px] bg-[#262525] relative z-10">
+      <div className="md:max-w-[1170px] md:m-auto md:relative px-4">
+        <div className="md:max-w-[1170px] md:m-auto h-px w-full bg-gradient-to-r from-transparent via-white to-transparent"></div>
+        <h3 className="mt-4 md:mt-[30px] font-['Plateia'] md:font-['Proxima-Nova'] md:text-[46px] text-left uppercase text-xl">
+          Logo
+        </h3>
+        <p className="font-['Proxima-Nova'] font-light md:font-bold text-base mt-6 md:mt-[30px] text-left">
+          {t("dedugin")}
+        </p>
+        <button
+          onClick={handleScrollToTop}
+          type="button"
+          className="bg-white  mt-4 h-[26px] flex items-center w-[128px] justify-center rounded rounded-s"
+        >
+          <img src={telegram} alt="Описание изображения" className="" />
         </button>
 
-        <button className="bg-white h-[26px] flex w-[26px] items-center justify-center rounded rounded-s">
-          <img src={vkontakte} alt="Описание изображения" />
-        </button>
-        <button className="bg-white h-[26px] flex items-center w-[26px] justify-center rounded rounded-s">
-          <img src={youtube} alt="Описание изображения" />
+        <div className="flex-row flex mt-[14px] gap-[10px]">
+          <button className="bg-white h-[26px] w-[26px] flex items-center justify-center rounded rounded-s">
+            <img src={instagram} alt="Описание изображения" />
+          </button>
+
+          <button className="bg-white h-[26px] flex w-[26px] items-center justify-center rounded rounded-s">
+            <img src={vkontakte} alt="Описание изображения" />
+          </button>
+          <div className="bg-white h-[26px] flex items-center w-[26px] justify-center rounded rounded-s">
+            <img src={youtube} alt="Описание изображения" />
+          </div>
+        </div>
+        <button
+          onClick={handleScrollToTop}
+          className="hidden md:block absolute top-[56px] right-[149px]"
+        >
+          <svg
+            width="60"
+            height="60"
+            viewBox="0 0 60 60"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="60" height="60" rx="4" fill="#7D6137" />
+            <rect
+              x="13.8385"
+              y="40.2246"
+              width="2.59999"
+              height="25.9999"
+              transform="rotate(-135 13.8385 40.2246)"
+              fill="white"
+            />
+            <rect
+              x="48.7692"
+              y="38.3848"
+              width="2.59999"
+              height="25.9999"
+              transform="rotate(135 48.7692 38.3848)"
+              fill="white"
+            />
+          </svg>
         </button>
       </div>
     </div>
