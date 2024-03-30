c55ddf2ae560aea3e8933b7ee244a5ab939dddbc
Artem <a.v.lyamichev@gmail.com>
2024-03-30 11:30:27 -0300

    fix: Slider

--- jetty-box/src/components/Slider.jsx
+++ jetty-box/src/components/Slider.jsx
@@ -1,11 +1,6 @@
 import { useTranslation } from "react-i18next";
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/free-mode";
 
-import { FreeMode } from "swiper/modules";
 import { ServiceData } from "./utils/constants.js";
 import planet from "../assets/planet-slider.svg";
 import income from "../assets/slide-income.svg";
@@ -26,42 +21,43 @@ export default function () {
         src={planet}
         className="md:hidden absolute right-0 top-[82px] overflow-[overlay]"
       />
-      <Swiper
-        breakpoints={{
-          375: {
-            slidesPerView: 3,
-            spaceBetween: 26,
-          },
-          400: {
-            slidesPerView: 3,
-            spaceBetween: 26,
-          },
-          600: {
-            slidesPerView: 4,
-            spaceBetween: 26,
-          },
-        }}
-        slidesPerView="auto"
-        freeMode={true}
-        modules={[FreeMode]}
-        className="gap-2 overflow-visible cursor-pointer md:hidden"
-      >
-        {ServiceData().map((item) => (
-          <SwiperSlide key={item.id} className=" !w-fit">
-            <div className="selector-shadow flex flex-col group relative rounded  text-white px-5 py-4 h-[268px] w-[187px] overflow-hidden ">
-              <div
-                className=" before:content-[''] before:absolute before:inset-0 before:block before:bg-gradient-to-tl before:from-[#202020] before:to-transparent before:z-[-5] z-10 bg-gradient-to-tl from-white to-black absolute inset-0 bg-cover bg-center"
-                style={{ backgroundImage: `url(${item.backgroundImage})` }}
-              />
-              <div className="flex w-full h-full">
-                <div className="relative justify-end flex flex-col">
-                  <p className="z-50  text-left">{item.content} </p>
+      <div className="md:hidden overflow-visible">
+        <div
+          className="slider-container"
+          style={{ overflowX: "auto", marginRight: "-20px" }}
+        >
+          <swiper-container
+            slides-per-view="auto"
+            spaceBetween={40}
+            scrollbar-clickable="true"
+            mousewheel-invert="true"
+            className="overflow-visible cursor-pointer"
+            style={{ cssMode: "true" }}
+          >
+            {ServiceData().map((item) => (
+              <swiper-slide
+                key={item.id}
+                style={{
+                  marginRight: "20px",
+                  width: "fit-content",
+                }}
+              >
+                <div className="mr-6 selector-shadow flex flex-col group relative rounded  text-white px-5 py-4 h-[268px] w-[187px] overflow-hidden">
+                  <div
+                    className="before:content-[''] before:absolute before:inset-0 before:block before:bg-gradient-to-tl before:from-[#202020] before:to-transparent before:z-[-5] z-10 bg-gradient-to-tl from-white to-black absolute inset-0 bg-cover bg-center"
+                    style={{ backgroundImage: `url(${item.backgroundImage})` }}
+                  />
+                  <div className="flex w-full h-full">
+                    <div className="relative justify-end flex flex-col">
+                      <p className="z-50  text-left">{item.content} </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+              </swiper-slide>
+            ))}
+          </swiper-container>
+        </div>
+      </div>
       <section className="hidden md:grid grid-cols-3 grid-rows-3 gap-x-[30px] gap-y-6">
         <div className="order-1 row-start-1 row-end-3 selector-shadow flex flex-col gap-6 group relative rounded  text-white px-5 py-4 h-[370px] max-w-[370px] overflow-hidden ">
           <div
