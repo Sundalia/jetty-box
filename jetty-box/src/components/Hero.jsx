990538fe461cfbd7a1688d15385b8d5547c72ca8
Artem <a.v.lyamichev@gmail.com>
2024-05-20 17:08:00 -0400

    feat: application link to data policy

--- jetty-box/src/components/Hero.jsx
+++ jetty-box/src/components/Hero.jsx
@@ -192,7 +192,13 @@ function Hero() {
                 <p className="mt-0 text-xs text-left">
                   {t("i-confirm")}{" "}
                   <span className="font-bold underline">
-                    {t("privacy-policy")}
+                    <a
+                      href="https://jvsqvggfg1pilypd.public.blob.vercel-storage.com/%D0%9F%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0%20%D0%B2%20%D0%BE%D1%82%D0%BD%D0%BE%D1%88%D0%B5%D0%BD%D0%B8%D0%B8%20%D0%BE%D0%B1%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B8%20%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-2jCFI8pxSwd4boriXQ0iO2GQVrZxti.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {t("privacy-policy")}
+                    </a>
                   </span>{" "}
                   {t("i-consent")}
                 </p>
